e5412377016632c7a5d6ecbb570f9d0daf934818
Niklas Ertle <hockeytertle@gmail.com>
2021-11-20 11:42:07 -0700

    Updated typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,7 +24,7 @@ const typeDefs = gql`
       _id: ID!
       title: String
       description: String
-      userId: [User]
+      userId: User
       dueDate: Int
       isComplete: Boolean
   }
@@ -51,10 +51,29 @@ const typeDefs = gql`
 
   type Query {
     users: [User]
+    projects(userId: ID!): [Project]
+    project(projectId: ID!): Project
+    me: User
   }
 
   type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+
+    createProject(title: String!, description: String!, startDate: Int, endDate: Int): Project
+    updateProject(projectId: ID!, title: String, description: String, startDate: Int, endDate: Int)
+    addUserToProject(projectId: ID!, userId: ID!): Project
+    removeProject(projectId: ID!)
+
+    addCalendar(projectId: ID!, title: String!, description: String!, dueDate: Int): Project
+    updateCalendar(projectId: ID!, title: String, description: String, dueDate: Int, isComplete: Boolean): Project
+    removeCalendar(projectId: ID!: Project
+
+    addKhanBan(projectId: ID!, title: String!, description: String!): Project
+    updateKhanBan(projectId: ID!, title: String, description: String, status: String): Project
+    removeKhanBan(projectId: ID!): Project
+
+    addMessage(message: String!): Message
   }
 `;
 
