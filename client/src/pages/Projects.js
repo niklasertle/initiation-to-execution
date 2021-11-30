0a98d317af80c1a64129e7a1dc5ad2896b895799
Niklas Ertle <hockeytertle@gmail.com>
2021-11-29 17:50:33 -0700

    Updated settings page. Displayed information to project page

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
-import ProjectNavbar from "../components/ProjectNavbar/Navbar";
+import ProjectNavbar from "../components/ProjectNavbar";
+import { useParams } from "react-router-dom";
 
 import Kanban from "../components/Kanban";
 import Messages from "../components/Messages";
 import Settings from "../components/Settings";
 
-export default function Projects() {
-  const [currentPage, setCurrentPage] = useState("KhanBan");
+import { useQuery } from "@apollo/client";
 
+import { GET_PROJECT } from "../utils/queries";
+
+export default function Projects() {
+  // Gets the project ID from the parameters
+  const { projectId } = useParams();
+
+  // Used to send the proper component
+  const [currentPage, setCurrentPage] = useState("Kanban");
+
+  // Get the project data from the database using the project ID
+  const { loading, data } = useQuery(GET_PROJECT, {
+    variables: { projectId: projectId },
+  });
+  const projectData = data?.project || [];
+
+  // console.log(projectData);
+
+  // If the user data hasn't been returned yet return loading
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  // Renders the selected component
   function renderPage() {
-    if ((currentPage === "Kanban")) {
-      return <Kanban />;
+    if (currentPage === "Kanban") {
+      return <Kanban kanban={projectData.kanban} projectId={projectId}/>;
     }
-    if ((currentPage === "Messages")) {
-      return <Messages />;
+    if (currentPage === "Messages") {
+      return <Messages messages={projectData.messages} projectId={projectId}/>;
     }
-    if ((currentPage === "Settings")) {
-      return <Settings />;
+    if (currentPage === "Settings") {
+      return <Settings users={projectData.users} projectId={projectId}/>;
     }
   }
 
   return (
     <>
-      <ProjectNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <h1>PROJECT TITLE HERE</h1>
-      {renderPage()}
+      <div className="m-3 ">
+        <ProjectNavbar
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+        <h1>{projectData.title}</h1>
+        <p>{projectData.description}</p>
+      </div>
+      <>{renderPage()}</>
     </>
   );
 }
