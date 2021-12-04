3467322c9ecdf0737e81fb3be1c4c461f647b3e4
Niklas Ertle <hockeytertle@gmail.com>
2021-12-04 13:03:54 -0700

    Updated chatroom to use firebase db

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCclhjtmMR06z3mczyuaXyIVsThi35Lss0",
+  authDomain: "i2e-app-e92f0.firebaseapp.com",
+  databaseURL: 'https://i2e-app-e92f0-default-rtdb.firebaseio.com/',
+  projectId: "i2e-app-e92f0",
+  storageBucket: "i2e-app-e92f0.appspot.com",
+  messagingSenderId: "86031308411",
+  appId: "1:86031308411:web:4b0c3c2c54dc080ee074de",
+  measurementId: "G-RFN018KD3N",
+};
+
+const fire = firebase.initializeApp(firebaseConfig)
+
+export default fire;
